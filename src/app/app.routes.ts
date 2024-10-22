66713abdc914e36798b7dc1ae84db824275a2370
davi <davibezerra123457@gmail.com>
2024-10-22 10:24:43 -0300

    feat: creates login and register routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import {DashboardComponent} from "./components/pages/dashboard/dashboard.component";
+import {LoginComponent} from "./components/pages/login/login.component";
+import {RegisterComponent} from "./components/pages/register/register.component";
 
 export const routes: Routes = [
   {path: '', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
 ];
