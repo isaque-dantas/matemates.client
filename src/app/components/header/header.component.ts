f243d797a826e049a64e9be1b38aa0c0dfb5a319
davi <davibezerra123457@gmail.com>
2024-10-22 09:00:57 -0300

    feat: generate header items

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
+import {KeyValuePipe, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLinkActive,
+    NgForOf,
+    RouterLink,
+    KeyValuePipe
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  navItems = [
+    {'label': 'Início', 'link': '/dashboard'},
+    {'label': 'Dashboard', 'link': '/buscar'},
+    {'label': 'Lorem', 'link': '/futureupdate'},
+  ]
 }
