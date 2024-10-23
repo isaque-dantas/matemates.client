be4bfb2a115174055ade63e7c13082dcd3328351
davi <davibezerra123457@gmail.com>
2024-10-23 07:31:56 -0300

    feat: dashboard complications

--- src/app/components/pages/dashboard/dashboard.component.ts
+++ src/app/components/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatIcon} from "@angular/material/icon";
+import {
+  MatAccordion, MatExpansionModule,
+  MatExpansionPanel,
+  MatExpansionPanelHeader,
+  MatExpansionPanelTitle
+} from "@angular/material/expansion";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-     MatIcon
+    MatIcon
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
