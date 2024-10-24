3f5bd93214260155109a38adc5ebfd4c00ac3a12
davi <davibezerra123457@gmail.com>
2024-10-24 15:39:19 -0300

    trying to fix layout

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,15 +1,17 @@
 import { Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  rememberMe:boolean = false;
 }
