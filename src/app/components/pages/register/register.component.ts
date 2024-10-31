63661f256d20dfd617c0bc80cb3bc2028288d5a6
davi <davibezerra123457@gmail.com>
2024-10-31 15:22:46 -0300

    feat: adds reactive form and validators to register

--- src/app/components/pages/register/register.component.ts
+++ src/app/components/pages/register/register.component.ts
@@ -1,16 +1,45 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AuthService} from "../../../services/auth.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-    imports: [
-        RouterLink
-    ],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent implements OnInit, OnDestroy {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      name: ['', [Validators.required, Validators.minLength(3)]]
+    })
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value).subscribe(
+        (response) => {
+          console.log('Registration successful', response)
+        },
+        (error) => {
+          console.error('Registration failed', error)
+        }
+      )
+    }
+  }
+
   ngOnInit() {
     document.body.style.overflow = 'hidden';
   }
