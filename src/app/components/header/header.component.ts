73645b6a1a5de52806556e610e54b7018c5f2deb
davi <davibezerra123457@gmail.com>
2024-11-01 11:18:02 -0300

    feat: adds logic to only display login and register buttons if user is not on profile.component.html route

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive} from "@angular/router";
-import {KeyValuePipe, NgForOf} from "@angular/common";
+import {Router, RouterLink, RouterLinkActive} from "@angular/router";
+import {KeyValuePipe, NgForOf, NgIf} from "@angular/common";
 import {MatButton} from "@angular/material/button";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-header',
@@ -11,16 +12,38 @@ import {MatButton} from "@angular/material/button";
     NgForOf,
     RouterLink,
     KeyValuePipe,
-    MatButton
+    MatButton,
+    NgIf
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 
 export class HeaderComponent {
+  showElement: boolean = true;
+
   navItems = [
     {'label': 'Início', 'link': ''},
     {'label': 'criar', 'link':   'create'},
     {'label': 'sugerir alteração', 'link': 'report'},
   ]
+
+  constructor(public authService: AuthService, private router: Router) {
+    this.router.events.subscribe(() =>{
+      const excludedRoutes = ['profile'];
+      this.showElement = !excludedRoutes.includes(this.router.url);
+    });
+  }
+
+  ngOnInit() {
+    this.updateShowElement();
+    this.router.events.subscribe(() => {
+      this.updateShowElement();
+    });
+  }
+
+  private updateShowElement() {
+    const excludedRoutes = ['/profile'];
+    this.showElement = !excludedRoutes.includes(this.router.url);
+  }
 }
