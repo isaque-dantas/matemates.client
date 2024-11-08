b7e8e576c6557f97388a1f6630c1ddec55a30894
davi <davibezerra123457@gmail.com>
2024-11-08 18:12:32 -0300

    feat: adds logic behind toasts in register page

--- src/app/components/pages/register/register.component.ts
+++ src/app/components/pages/register/register.component.ts
@@ -17,11 +17,15 @@ import {NgIf} from "@angular/common";
 })
 export class RegisterComponent implements OnInit, OnDestroy {
   registerForm: FormGroup;
+  showPasswordToast: false | undefined | boolean;
+  showUserToast: false | undefined | boolean;
+  showEmailToast: false | undefined | boolean;
+  showNameToast: false | undefined | boolean;
 
   constructor(private fb: FormBuilder, private authService: AuthService) {
     this.registerForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
       username: ['', [Validators.required, Validators.minLength(3)]],
       name: ['', [Validators.required, Validators.minLength(3)]],
       rememberMe: [false]
@@ -43,6 +47,34 @@ export class RegisterComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     document.body.style.overflow = 'hidden';
+
+    this.registerForm.get('username')?.valueChanges.subscribe(() => {
+      this.showUserToast = this.registerForm.get('username')?.invalid && this.registerForm.get('username')?.touched;
+      if (this.showUserToast) {
+        setTimeout(() => this.showUserToast = false, 5000);
+      }
+    });
+
+    this.registerForm.get('password')?.valueChanges.subscribe(() => {
+      this.showPasswordToast = this.registerForm.get('password')?.invalid && this.registerForm.get('password')?.touched;
+      if (this.showPasswordToast) {
+        setTimeout(() => this.showPasswordToast = false, 5000);
+      }
+    });
+
+    this.registerForm.get('email')?.valueChanges.subscribe(() => {
+      this.showEmailToast = this.registerForm.get('email')?.invalid && this.registerForm.get('email')?.touched;
+      if (this.showEmailToast) {
+        setTimeout(() => this.showEmailToast = false, 5000);
+      }
+    });
+
+    this.registerForm.get('name')?.valueChanges.subscribe(() => {
+      this.showNameToast = this.registerForm.get('name')?.invalid && this.registerForm.get('name')?.touched;
+      if (this.showNameToast) {
+        setTimeout(() => this.showNameToast = false, 5000);
+      }
+    });
   }
 
   ngOnDestroy() {
