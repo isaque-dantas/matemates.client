d2db9981b7c60b3f8afb5748a9f459355c0f92af
isaque-dantas <vicisaque413@gmail.com>
2024-11-14 14:51:34 -0300

    fix: authService validates token time

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable, tap} from "rxjs";
 import {LoginData, UserToSend} from "../interfaces/user";
@@ -12,13 +12,14 @@ export class AuthService {
   private baseUrl = 'http://127.0.0.1:8000/api/users'
   private tokenUrl = 'http://127.0.0.1:8000/api/token'
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {
+  }
 
   login(credentials: LoginData): Observable<any> {
     console.log('Logging in with:', credentials);
-    return this.http.post<{access: string, refresh: string, userId: number}>(`${this.tokenUrl}`, credentials).pipe(
+    return this.http.post<{ access: string, refresh: string, userId: number }>(`${this.tokenUrl}`, credentials).pipe(
       tap(response => {
-        if(response.access) {
+        if (response.access) {
           localStorage.setItem('access', response.access);
           this.router.navigate(['']);
         }
@@ -37,7 +38,27 @@ export class AuthService {
     this.router.navigate(['login']);
   }
 
-  isAuthenticated():boolean {
+  isAuthenticated(): boolean {
+    return this.doesAccessTokenExist() && !this.isAccessTokenExpired()
+  }
+
+  isAccessTokenExpired(): boolean {
+    if (!this.doesAccessTokenExist()) return false
+
+    const token: string = localStorage.getItem('access')!
+    const encodedTokenPayload = token.split('.')[1]
+    const decodedTokenPayload = JSON.parse(atob(encodedTokenPayload))
+    const exp = decodedTokenPayload.exp
+
+    if (!exp) return false
+
+    const expirationDate = new Date(exp * 1000).getTime()
+    const currentDate = new Date().getTime()
+
+    return currentDate >= expirationDate
+  }
+
+  doesAccessTokenExist() {
     return !!localStorage.getItem('access');
   }
 
