56fb70dac49b4a0f03d9f8c83f31d82df4c57e8a
davi <davibezerra123457@gmail.com>
2025-02-04 09:11:35 -0300

    feat: removes edit button if user is not admin

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,7 @@ import {Router, RouterLink, RouterLinkActive} from "@angular/router";
 import {NgForOf, NgIf} from "@angular/common";
 import {MatButton} from "@angular/material/button";
 import {AuthService} from "../../services/auth.service";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-header',
@@ -20,14 +21,11 @@ import {AuthService} from "../../services/auth.service";
 
 export class HeaderComponent {
   showElement: boolean = true;
+  userData: any;
+  token: any;
+  navItems: any = [];
 
-  navItems = [
-    {'label': 'Início', 'link': ''},
-    {'label': 'Criar', 'link': '/create_entry'},
-    {'label': 'Sugerir Alteração', 'link': '/nae'},
-  ]
-
-  constructor(public authService: AuthService, private router: Router) {
+  constructor(public authService: AuthService, private router: Router, private userService: UserService) {
     this.router.events.subscribe(() => {
       const excludedRoutes = ['profile'];
       this.showElement = !excludedRoutes.includes(this.router.url);
@@ -39,6 +37,27 @@ export class HeaderComponent {
     this.router.events.subscribe(() => {
       this.updateShowElement();
     });
+
+    this.userService.getUserData(this.token).subscribe(
+      (data) => {
+        this.userData = data;
+
+        if (this.userData.is_staff) {
+          this.navItems = [
+            {'label': 'Início', 'link': ''},
+            {'label': 'Criar', 'link': '/create_entry'},
+            {'label': 'Sugerir Alteração', 'link': '/nae'},
+          ]
+        } else {
+          this.navItems = [
+            {'label': 'Início', 'link': ''},
+            {'label': 'Sugerir Alteração', 'link': '/nae'},
+          ]
+        }
+
+        console.log('User Data:', this.userData);
+      }
+    )
   }
 
   private updateShowElement() {
